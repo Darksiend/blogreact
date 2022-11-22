ce07493ab548a6b3ab3a9c64aa783b796290514b
Darksiend <logosmk2@gmail.com>
2022-11-22 09:47:18 +0200

    Work

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 const port = 4444;
 const app = express();
-
+mongoose
+  .connect(
+    "mongodb+srv://darksiend:123@mycluster.eswzs4i.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB Ok!");
+  })
+  .catch((e) => console.log("DB Error", e));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.use(express.json());
 
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-  const token = jwt.sign({email: req.body.email, fullName:'Anton Chesnokov'})
-  res.json({ success: true });
-});
+app.post("/auth/register", (req, res) => {});
 
 app.listen(port, (e) => {
   if (e) throw e;
